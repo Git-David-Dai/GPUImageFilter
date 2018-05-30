37f3ed499a66b8f65c6029c259c170b4be4632bb
David Dai <david.dai@meitus-macbook-pro.local>
2018-05-30 15:40:07 +0800

    Initial Commit

---
+++ GPUImageFilter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GPUImageFilter
+//
+//  Created by David.Dai on 18/5/30.
+//  Copyright © 2018年 David.Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GPUImageFilter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GPUImageFilter
+//
+//  Created by David.Dai on 18/5/30.
+//  Copyright © 2018年 David.Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
