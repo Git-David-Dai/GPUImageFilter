8f653bb4e9e41ee5a0994f1ffe3e3932abb94a2d
David.Dai <david.dai@meitus-MacBook-Pro.local>
2018-06-01 13:52:33 +0800

    first commit

---
+++ GPUImageFilter/GPUImage3DFilter.h
@@ -0,0 +1,13 @@
+//
+//  GPUImage3DFilter.h
+//  GPUImageFilter
+//
+//  Created by David.Dai on 18/5/30.
+//  Copyright © 2018年 David.Dai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GPUImage3DFilter : NSObject
+
+@end
---
+++ GPUImageFilter/GPUImage3DFilter.m
@@ -0,0 +1,13 @@
+//
+//  GPUImage3DFilter.m
+//  GPUImageFilter
+//
+//  Created by David.Dai on 18/5/30.
+//  Copyright © 2018年 David.Dai. All rights reserved.
+//
+
+#import "GPUImage3DFilter.h"
+
+@implementation GPUImage3DFilter
+
+@end
